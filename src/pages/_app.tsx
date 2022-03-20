5a4a4ae07715fc4e3b869f9ca49508f32184fb77
Lucas C <LucasCondeFerreira@gmail.com>
2022-03-20 00:39:02 -0300

    Mobile 1.03v

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Gabi Designns</title>
-        // Importe da fonte Quicksand
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        // Importe da fonte LEMON MILK Medium
         <link href="http://fonts.cdnfonts.com/css/lemonmilk" rel="stylesheet" />
       </Head>
       <ThemeProvider theme={theme}>
