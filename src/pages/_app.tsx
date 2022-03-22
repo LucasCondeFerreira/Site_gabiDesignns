4a8f5d5f503f2da369dfce20759c86ed7acdf9fd
Lucas C <LucasCondeFerreira@gmail.com>
2022-03-22 20:07:04 -0300

    Mobile and Desk 1.14v | Otimizando codigo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import theme from "@themes/theme";
 import Head from "next/head";
+import Range from "@components/esthetic/range/Range";
+import Header, { HeaderWhite } from "@components/surface/header/Header.jsx";
 import { RangeBottom } from "@components/esthetic/range/Range";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -15,6 +17,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider theme={theme}>
+        <Range />
+        <Header />
         <Component {...pageProps} />
         <RangeBottom />
       </ThemeProvider>
